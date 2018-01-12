c31d254549c44531cde9d620e816388d03d204de
DESKTOP-RP133MO\Benedikt <froeschl.benedikt@hakmistelbach.ac.at>
2018-01-12 09:45:55 +0100

    Klasse Kassa fertig

--- Lager.cs
+++ Lager.cs
@@ -8,9 +8,17 @@ namespace Verkauf
 {
     class Lager
     {
-        public Lager( )
+        public Lager(long lid, long pid, long kid, long mid )
         {
-
+            LagerID= lid;
+            ProduktID = pid;
+            KID = kid;
+            MID = mid;
         }
+
+        public long LagerID { get; set; }
+        public long ProduktID { get; set; }
+        public long KID { get; set; }
+        public long MID { get; set; }
     }
 }
